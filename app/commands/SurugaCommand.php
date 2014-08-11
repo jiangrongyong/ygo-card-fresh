67ee026c1f73e480fc1fb76842fbbf089ce24e6c
jiangrongyong <jiangrongyong@gmail.com>
2014-08-11 17:12:29 +0800

    fix

--- app/commands/SurugaCommand.php
+++ app/commands/SurugaCommand.php
@@ -53,7 +53,7 @@ class SurugaCommand extends Command {
             }
         }
 
-        Mail::send('emails.suruga', compact('notifications'), function ($message) {
+        Mail::send('emails.suruga.index', compact('notifications'), function ($message) {
             $message->to('jiangrongyong@gmail.com', 'Hugh')->subject('Suruga Updates');
         });
     }
